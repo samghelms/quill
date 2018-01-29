be4cb45f7c3d4961e11d4844e159e193a446c68b
Sam <samghelms@gmail.com>
2018-01-28 21:19:32 -0500

    added this.domNode.setAttribute('data-value', newVal) to the inputHandler, since quill.getContents() seems to rely on the this.domNode data-value attribute

--- formats/formula.js
+++ formats/formula.js
@@ -1,86 +1,90 @@
-import Embed from '../blots/embed';
+import Embed from '../blots/embed'
 
 class Formula extends Embed {
   static create(value) {
-    let node = super.create(value);
-    node.style.position = "relative";
-    let mathNode = document.createElement("div");
-    mathNode.style.display = "inline-block";
-    mathNode.className = "mathNode";
+    let node = super.create(value)
+    node.style.position = "relative"
+    let mathNode = document.createElement("div")
+    mathNode.style.display = "inline-block"
+    mathNode.className = "mathNode"
     if (typeof value === 'string') {
-      node.setAttribute('data-value', value);
+      node.setAttribute('data-value', value)
     }
-    node.appendChild(mathNode);
-    return node;
+    node.appendChild(mathNode)
+    return node
   }
   constructor(value) {
-    super(value);
+    super(value)
     let mathNode = getMathNode(this.domNode)
     this.renderMath(value.getAttribute("data-value"), mathNode)
-    mathNode.addEventListener('click', (() => this.edit([-1])));
+    mathNode.addEventListener('click', (() => this.edit([-1])))
   }
   editContainer() {
-    var container = document.createElement("div");
-    container.className = "editContainer";
-    container.style.display = "inline-block";
-    return container;
+    var container = document.createElement("div")
+    container.className = "editContainer"
+    container.style.display = "inline-block"
+    return container
   }
   enter(args) {
     this.edit(args)
   }
   exit() {
+
+    /* These indicing assumptions are safe since we created all of the elements
+    in other parts of this class*/
     let parent = this.domNode.children[0]
-    let editContainers = parent.getElementsByClassName("editContainer");
+    let editContainers = parent.getElementsByClassName("editContainer")
     let editContainer = editContainers[0]
     let mathNode = getMathNode(editContainer)
     parent.replaceChild(mathNode, editContainer)
   }
   edit(args) {
-    let mathNodes = this.domNode.getElementsByClassName("mathNode");
+    let mathNodes = this.domNode.getElementsByClassName("mathNode")
     if (mathNodes.length !== 1) {
       throw new Error()
     }
-    let mathNode = mathNodes[0];
+    let mathNode = mathNodes[0]
     let mathNodeParent = mathNode.parentNode
-    let editContainer = this.editContainer();
-    mathNodeParent.removeChild(mathNode);
-    editContainer.appendChild(mathNode);
-    mathNodeParent.appendChild(editContainer);
-    let editArea = this.getEditor();
-    let editAreaContainer = document.createElement("div");
+    let editContainer = this.editContainer()
+    mathNodeParent.removeChild(mathNode)
+    editContainer.appendChild(mathNode)
+    mathNodeParent.appendChild(editContainer)
+    let editArea = this.getEditor()
+    let editAreaContainer = document.createElement("div")
     editAreaContainer.appendChild(editArea)
     let val = this.contentNode.getAttribute("data-value")
-    editArea.value = val;
-    editContainer.appendChild(editAreaContainer);
-    editArea.focus();
+    editArea.value = val
+    editContainer.appendChild(editAreaContainer)
+    editArea.focus()
+
     const [entryIndex, initialFlag] = args
-    if(entryIndex>window.quill.getIndex(this)&&initialFlag!=="initial") {
-      editArea.setSelectionRange(0, 0);
+    if(entryIndex > window.quill.getIndex(this) && initialFlag !== "initial") {
+      editArea.setSelectionRange(0, 0)
     } else if (initialFlag === "initial") {
-      editArea.setSelectionRange(0, 2);
+      editArea.setSelectionRange(0, 2)
     }
   }
   getEditor() {
-    let editArea = document.createElement("textarea");
-    editArea.style.position = "absolute";
-    editArea.style.minWidth = "100px";
-    editArea.style.width = "100%";
-    editArea.style.outline = "none";
-    editArea.style.resize = "none";
-    editArea.addEventListener('input', (e) => this.inputHandler(e, this.domNode))
+    let editArea = document.createElement("textarea")
+    editArea.style.position = "absolute"
+    editArea.style.minWidth = "100px"
+    editArea.style.width = "100%"
+    editArea.style.outline = "none"
+    editArea.style.resize = "none"
+    editArea.addEventListener('input', (el) => this.inputHandler(el))
     editArea.addEventListener("keydown", (e) => keyHandler(e, this))
     editArea.addEventListener('blur', () => this.exit())
-    return editArea;
+    return editArea
   }
   renderMath(value, mathNode) {
-    const processedValue = this.preprocessor(value);
+    const processedValue = this.preprocessor(value)
     try {
       window.katex.render(processedValue, mathNode, {
         throwOnError: false,
         errorColor: '#f00'
       })
     } catch(err) {
-      mathNode.innerHTML = err.message;
+      mathNode.innerHTML = err.message
     }
   }
   preprocessor(value) {
@@ -89,47 +93,49 @@ class Formula extends Embed {
        Allows for things like adding \begin{aligned} tags
        or using Asciimath.
     */
-    return value;
+    return value
   }
-  inputHandler(e, domNode) {
+  inputHandler(e) {
     let newVal = e.srcElement.value
-    let mathNode = getMathNode(domNode)
+    let mathNode = getMathNode(this.domNode)
 
     this.renderMath(newVal, mathNode)
-    domNode.setAttribute('data-value', newVal);
+    this.contentNode.setAttribute('data-value', newVal)
+    this.domNode.setAttribute('data-value', newVal)
   }
   static value(domNode) {
-    const val = domNode.getAttribute('data-value');
-    return val;
+    const val = domNode.getAttribute('data-value')
+    return val
   }
 }
-Formula.blotName = 'formula';
-Formula.className = 'ql-formula';
-Formula.tagName = 'SPAN';
+Formula.blotName = 'formula'
+Formula.className = 'ql-formula'
+Formula.tagName = 'SPAN'
+
 const keyHandler = (e, ctx) => {
-  const selectionLength = e.srcElement.value.length;
-  const cursorLocation = e.srcElement.selectionEnd;
-  const key = e.key;
+  const selectionLength = e.srcElement.value.length
+  const cursorLocation = e.srcElement.selectionEnd
+  const key = e.key
   let curIndex = window.quill.getIndex(ctx)
   if (cursorLocation <= 0 && key === "ArrowLeft") {
     window.quill.setSelection(5, 1, "user")
     window.quill.setSelection(curIndex, 1, "user")
-    ctx.exit(e.srcElement)
   }
   if (cursorLocation >= selectionLength && key === "ArrowRight") {
     curIndex = window.quill.getIndex(ctx)
     window.quill.setSelection(curIndex+1, 1, "user")
-    ctx.exit(e.srcElement)
   }
 
 }
+
 const getMathNode = (domNode) => {
   let mathNodes = domNode.getElementsByClassName("mathNode")
   if (mathNodes.length !== 1) {
-    var err = new Error();
+    var err = new Error("should not have multiple math classes in the same embed")
     throw err
   } else {
     return mathNodes[0]
   }
 }
-export default Formula;
+
+export default Formula
